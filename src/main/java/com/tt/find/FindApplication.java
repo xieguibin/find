48380adb356ad7dbfcea1d38e6ffbb5d5365f1c0
TSTank <m18903577547@163.com>
2018-12-22 08:32:52 +0800

    我的天啊，这竟然是我写的项目

---
+++ src/main/java/com/tt/find/FindApplication.java
@@ -0,0 +1,14 @@
+package com.tt.find;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindApplication.class, args);
+	}
+
+}
+
